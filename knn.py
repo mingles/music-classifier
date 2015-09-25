706b253cb32ff00a51498f56590d25920cf7b319
Michael Inglis <michaelinglis93@googlemail.com>
2015-09-25 17:42:54 +0100

    Reformatted in Classes
    Cross Validation suitable for different classifiers.

---
+++ knn.py
@@ -0,0 +1,61 @@
+__author__ = 'mingles'
+
+import numpy as np
+
+
+class Knn():
+
+    def __init__(self, k, test_val, training_set, training_classes):
+        self.k = k
+        self.test_val = test_val
+        self.training_set = training_set
+        self.training_classes = training_classes
+        self.nearest_neighbour_class = [[-1]]
+        self.distance_values = []
+        self.nearest_classes = []
+
+    def get_nn(self):
+
+        # find distances between test value and all points in training set
+        for y in range(0, len(self.training_set)):
+            training_val = self.training_set[y]
+            distance = np.linalg.norm(np.array(abs(self.test_val - training_val)))
+            self.distance_values.append((y, distance))
+
+        # sort get classes by closest distance values and pick first k
+        data_type = [('position', int), ('difference', object)]
+        self.distance_values = np.sort(np.array(self.distance_values, dtype=data_type), order='difference')
+        for distance in self.distance_values[:self.k]['position']:
+            self.nearest_classes.append(self.training_classes[distance])
+
+        # return mode of distance values
+        return self.knn_mode()
+
+    # Finds the mode. If >1 classes have the greatest number of points in the
+    # k nearest neighbours, the furthest point is removed until the tie is broken.
+    def knn_mode(self):
+
+        # count occurrences of classes
+        done = []
+        the_result = []
+        for a in self.nearest_classes:
+            count = 0
+            for b in self.nearest_classes:
+                if a not in done:
+                    if a == b:
+                        count += 1
+            done.append(a)
+            the_result.append((a, count))
+
+        # find most common occurrence and break ties
+        max_occurrence = 0
+        mode = -1
+        for c in the_result:
+            if c[1] > max_occurrence:
+                max_occurrence = c[1]
+                mode = c[0]
+            elif c[1] == max_occurrence:
+                # remove furthest point and repeat function
+                self.nearest_classes = self.nearest_classes[:(len(self.nearest_classes) - 1)]
+                self.knn_mode()
+        return mode
--- test.py
+++ test.py
@@ -1,35 +1,69 @@
 __author__ = 'mingles'
+import scipy.io
+import knn
 
-import numpy as np
 
-def knn_mode(nearest_classes):
-    done = []
-    the_result = []
-    for a in nearest_classes:
-        count = 0
-        for b in nearest_classes:
-            if a not in done:
-                if a == b:
-                    count += 1
-        done.append(a)
-        the_result.append((a, count))
+class CrossValidate():
 
-    max_occurance = 0
-    mode = -1
-    for c in the_result:
-        if c[1] > max_occurance:
-            max_occurance = c[1]
-            mode = c[0]
-        elif c[1] == max_occurance:
-            # "remove end and redo"
-            nearest_classes = nearest_classes[:(len(nearest_classes) - 1)]
-            knn_mode(nearest_classes)
-    return mode
+    def __init__(self, classifier):
+        print "Cross Validating with Classifier " + str(classifier)
+        data = scipy.io.loadmat("gtzan.mat")
+        features = []
+        classes = []
+        classifier = classifier
+
+        for i in (range(1, 11)):
+            features.append(data['fold%d_features' % i])
+            classes.append(data['fold%d_classes' % i])
+
+        correctly_classified = 0
+        total = 0
+        for folds in features:
+            total += (len(folds))
+
+        # iterating through each fold
+        for i in range(len(features)):
+            correctly_classified_fold = 0
+            test_set = features[i]
+            test_classes = classes[i]
+
+            training_set = []
+            training_classes = []
+
+            # creating training set for fold i
+            for j in range(len(features)):
+                if i != j:
+                    for k in range(len(features[j])):
+                        training_set.append(features[j][k])
+                        training_classes.append(classes[j][k])
+
+            # classifying the test_set against the training_set
+            for x in range(len(test_set)):
+                test_val = test_set[x]
+                test_class = test_classes[x]
+                classified_class = [[-1]]
+
+                # create classifier
+                if len(classifier) > 1 and classifier[0] == "knn":
+                    k = knn.Knn(classifier[1], test_val, training_set, training_classes)
+                    classified_class = k.get_nn()
+                else:
+                    print "Invalid Classifier Input"
+                    quit()
+
+                # checking if test_data was classified correctly
+                if classified_class == test_class:
+                    correctly_classified_fold += 1
+
+            print "Testing Fold " + str(i + 1) + ": " + str(correctly_classified_fold) + "/" + str(len(test_classes))
+            correctly_classified = correctly_classified + correctly_classified_fold
+
+        print "Combined Result: " + str(correctly_classified) + "/" + str(total)
+        print "Percentage Correctly Classified: " + str(("{0:.2f}".format(float(correctly_classified) / float(total))))
+
 
 def main():
-    grades = [62, 73, 70, 64, 56, 72, 67, 58, 74, 64, 72]
-    print np.median(grades)
-    print np.average(grades)
-    print np.std(grades)
+    CrossValidate(("knn", 1))
+
 if __name__ == '__main__':
     main()
